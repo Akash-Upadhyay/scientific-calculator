1a73afff9b857a8c2221f0746b5e7343bb275c9b
Akash Upadhyay <akashupadhyay629@gmail.com>
2025-02-20 13:37:12 +0530

    modify entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+clear  # Clears the screen for a fresh start
+echo "Scientific Calculator"
+echo "1. Square Root (√x)"
+echo "2. Factorial (x!)"
+echo "3. Natural Log (ln(x))"
+echo "4. Power Function (x^b)"
+echo "5. Exit"
 java -jar /app/scientific-calculator.jar
 
 
